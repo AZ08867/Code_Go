ffd352b645f6092ca300c645f77c8cb52aa900a3
AZKvgkv <153256707+AZKvgkv@users.noreply.github.com>
2024-08-21 15:48:45 +0800

    easyjson

---
+++ src/ch35/easyjson/struct_def.go
@@ -0,0 +1,18 @@
+package easy_json
+
+type BasicInfo struct {
+	Name    string `json:"name"`
+	Age     int    `json:"age"`
+	Address string `json:"address"`
+}
+
+type JobInfo struct {
+	Skills   []string `json:"skills"`
+	Company  string   `json:"company"`
+	Position string   `json:"position"`
+}
+
+type Employee struct {
+	BasicInfo BasicInfo `json:"basic_info"`
+	JobInfo   JobInfo   `json:"job_info"`
+}
